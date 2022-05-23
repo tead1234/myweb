a5424df824e864b071dcd261c838f7bc00cae292
tead1234 <tead1234@gmail.com>
2022-05-23 23:00:28 +0900

    에라모르겠다

--- JS/todaysQuestion.js
+++ JS/todaysQuestion.js
@@ -58,18 +58,17 @@ myStorage = window.localStorage;
 // 진행도 보이기/ 닫기
 $('#open-question-bar').click(()=>{
     if($('.question-bar').css('display')== 'none'){
-    $('.question-bar').css('display','inline');
-    $('.main-question-screen').css('width','80%');
-    }
+    $('.main-question-screen').css('width', '80%')
+$('.question-bar').css('display','inline');
+}
     else{
+        $('.main-question-screen').animate({width: '100%'}, 500)
         $('.question-bar').css('display','none');
-        $('.main-question-screen').css('width','100%');
     }
 })
 
 $("#submitAnswer").click(()=>{
     let answer = $("#answer-board").val();
-    
     // 답안이랑 answer_OS[i]가 문제ㄹ를 말함 
     if (key == 0){
         답안리스트 = answer_OS
@@ -97,7 +96,6 @@ $("#submitAnswer").click(()=>{
         if(saveAnswer(질문리스트[rand],answer)){
             return
         }
-        passOrfail();
         //  답안리스트 == 이놈은 답안
         
         /// 맞추면 맞춘 갯수를 저장하는건 여기다가 따로 만들어야 할듯
@@ -106,15 +104,16 @@ $("#submitAnswer").click(()=>{
         countAnswer();
         update();
     }else{
-        alert("점수 미달입니다.");
+        //alert("점수 미달입니다.");
+        $("#answer-board").html("틀렸음")
+        $(".black-background").css('transform','translateY(1400px)')
+
         update_fail();
     }
 
     score = 0
 })
-function passOrfail(){
-   alert("축하합니다 통과하셨습니다.")
-}
+
 function saveAnswer(문항, 제출정답){
     // 통과된 문항기록
     if(sucess_answer.includes(문항)){
